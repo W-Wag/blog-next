d6519a46dc7aa7210687553a967010c55f1675d8
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-12-12 14:14:12 -0300

    Aula configurando os query params do getAllPosts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { getAllPosts } from '@/data/posts/get-all-posts';
 import { GetPostsData } from '@/domain/post/types';
 
 export default async function Home() {
-  const posts: GetPostsData = await getAllPosts();
+  const posts: GetPostsData = await getAllPosts(
+    'sort=id:desc&pagination[start]=0&pagination[limit]=10',
+  );
   return <HomePage posts={posts} />;
 }
--- src/data/posts/get-all-posts.ts
+++ src/data/posts/get-all-posts.ts
@@ -1,5 +1,6 @@
-export const getAllPosts = async () => {
-  const res = await fetch(`${process.env.POSTS_URL}`);
+export const getAllPosts = async (query = '') => {
+  const url = `${process.env.POSTS_URL}?populate=deep&${query}`;
+  const res = await fetch(url);
   if (!res.ok) {
     console.log(res.status);
   }
